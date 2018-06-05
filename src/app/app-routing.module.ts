d963251eefe654e8ca9aec3640349cc5b0ab71c5
tresdosdos <vitaliystrelok8@gmail.com>
2018-06-05 19:37:55 +0300

    fiiixxxx

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const appRoutes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, {useHash: true})
   ],
   exports: [RouterModule],
   declarations: []
