2b5759b5760f3811b758139e08ffc0fee5a100e9
tresdosdos <vitaliystrelok8@gmail.com>
2018-06-05 19:49:30 +0300

    ss

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/app-store'));
 
 app.listen(process.env.PORT || 5000);
 
-app.get("*", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname +'/dist/app-store/index.html'));
 });
 
