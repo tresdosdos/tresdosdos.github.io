1bb3c774c20384346273d19c2d1dccab881522e0
tresdosdos <vitaliystrelok8@gmail.com>
2018-06-05 19:34:01 +0300

    f

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('/dist/app-store'));
+app.use(express.static(__dirname + '/dist/app-store'));
 
 
 app.listen(process.env.PORT || 5000);
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join('/dist/app-store/index.html'));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname +'/dist/app-store/index.html'));
 });
 
 console.log('Console listening on port ' + process.env.PORT);
