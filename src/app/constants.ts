b452b9e9fe122661bd3d756fc2a5da69175559b3
tresdosdos <vitaliystrelok8@gmail.com>
2018-06-05 17:47:29 +0300

    const

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -4,4 +4,4 @@ export const KEYBOARD = {
 
 export const CLIENT_ID = 'e538cfcbe2e6441ca9575cc7e7873b62';
 
-export const REDIRECT_URI = 'https://smartextask-app-store.herokuapp.com/';
+export const REDIRECT_URI = 'https://smartextask-app-store.herokuapp.com:5000';
