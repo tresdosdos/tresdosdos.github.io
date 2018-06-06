58cfcd4f87e15210e86c630f6a8e01aa232c1aa1
tresdosdos <vitaliystrelok8@gmail.com>
2018-06-06 11:41:42 +0300

    prelast fixes

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const CLIENT_ID = 'e538cfcbe2e6441ca9575cc7e7873b62';
+const CLIENT_SECRET = '506a292cb82940f885fa28dc30e6ef70';
+
+const REDIRECT_URI = 'https://smartextask-app-store.herokuapp.com';
 
 app.use(express.static(__dirname + '/dist/app-store'));
 
+app.use(bodyParser.urlencoded({ extended: true })); ///нужное
+app.use(bodyParser.json());
 
 app.listen(process.env.PORT || 5000);
+
+app.post('/gettoken', function (req, res) {
+  const data = {'client_id' : CLIENT_ID,
+    'client_secret' : CLIENT_SECRET,
+    'grant_type' : 'authorization_code',
+    'redirect_uri' : REDIRECT_URI,
+    'code' : req.body.code
+  };
+
+// Configure the request
+  const options = {
+    uri: 'https://api.instagram.com/oauth/access_token',
+    method: 'POST',
+    form: data
+  };
+  request(options, function (error, response, body) {
+
+    // to convert the string body to a usable object
+    const pbody = JSON.parse(body);
+    console.log(pbody);
+    res.send(pbody);
+  });
+});
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname +'/dist/app-store/index.html'));
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-import { CLIENT_ID, REDIRECT_URI } from '../constants';
+import {CLIENT_ID, CLIENT_SECRET, REDIRECT_URI} from '../constants';
 import {Observable} from 'rxjs';
 import {User} from './user';
 import {USERINFO} from './user-info';
 import {APPS} from './apps';
 import {GetDataService} from './get-data.service';
+import { URLSearchParams } from 'url';
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +15,16 @@ import {GetDataService} from './get-data.service';
 export class AuthService {
   logIn(): void {
     window.location.href =
-      `https://api.instagram.com/oauth/authorize/?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=token`;
+      `https://api.instagram.com/oauth/authorize/?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   }
   getToken(): Observable<string> {
     return this.route.fragment;
+  }
+  getCode(): string {
+    let url = window.location.href;
+    url = url.slice(url.indexOf('code') + 5, url.length - 2);
+    console.log(url);
+    return url;
   }
   getUserData(token): Observable<any> {
       this.saveLocalToken(token);
@@ -25,6 +32,7 @@ export class AuthService {
   }
   saveLocalToken(token): void {
     localStorage.setItem('appStoreToken', token);
+    console.log(localStorage.appStoreToken);
   }
   getLocalToken(): string {
     return localStorage.getItem('appStoreToken');
@@ -56,15 +64,27 @@ export class AuthService {
     });
   }
   remoteTokenFetch(): void {
-    this.getToken().subscribe(token => {
-      console.log(token);
-      if (token) {
-        token = token.slice(13, token.length);
-        this.getUserData(token).subscribe(userData => {
-          this.setUserData(userData);
-          console.log('fetching');
-        });
-      }
+    // this.getToken().subscribe(token => {
+    //   console.log(token);
+    //   if (token) {
+    //     token = token.slice(13, token.length);
+    //     this.getUserData(token).subscribe(userData => {
+    //       this.setUserData(userData);
+    //       console.log('fetching');
+    //     });
+    //   }
+    // });
+    const code = this.getCode();
+    // const search = new URLSearchParams();
+    // search.set('client_id', CLIENT_ID);
+    // search.set('client_secret', CLIENT_SECRET);
+    // search.set('redirect_uri', REDIRECT_URI);
+    // search.set('code', code);
+    // search.set('grant_type', 'authorization_code');
+    fetch('/gettoken', {method: 'post', headers: {'Content-Type': 'application/json'
+      }, body: JSON.stringify({code: code})}).then(res => res.json()).then(res => {
+        this.saveLocalToken(res.access_token);
+        this.setUserData({data: res.user});
     });
   }
   deleteLocalToken(): void {
