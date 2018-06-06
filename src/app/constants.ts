eea627a515e76da2e3dde7572aeb958d9c400f76
tresdosdos <vitaliystrelok8@gmail.com>
2018-06-06 17:47:56 +0300

    fix constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -4,5 +4,5 @@ export const KEYBOARD = {
 
 export const CLIENT_ID = 'e538cfcbe2e6441ca9575cc7e7873b62';
 
-//export const REDIRECT_URI = 'https://smartextask-app-store.herokuapp.com';
-export const REDIRECT_URI = 'http://localhost:5000';
+export const REDIRECT_URI = 'https://smartextask-app-store.herokuapp.com';
+//export const REDIRECT_URI = 'http://localhost:5000';
