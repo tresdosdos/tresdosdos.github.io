85b54cb184f7a352ca9fb123c41d6675dbe33524
tresdosdos <vitaliystrelok8@gmail.com>
2018-06-12 16:06:21 +0300

    a

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -4,8 +4,8 @@ export const KEYBOARD = {
 
 export const CLIENT_ID = 'e538cfcbe2e6441ca9575cc7e7873b62';
 
-//export const REDIRECT_URI = 'https://smartextask-app-store.herokuapp.com';
-export const REDIRECT_URI = 'http://localhost:5000';
+export const REDIRECT_URI = 'https://smartextask-app-store.herokuapp.com';
+//export const REDIRECT_URI = 'http://localhost:5000';
 
 export const ACCESS_TOKEN_URL = 'https://api.instagram.com/v1/users/self/?access_token=';
 
