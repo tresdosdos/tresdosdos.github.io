b8bc4a60dd87754e2b5a2598d855ece5c110bc34
tresdosdos <vitaliystrelok8@gmail.com>
2018-10-16 09:27:52 +0300

    deploy

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 2000;
+  await app.listen(port);
+
+  process.on('SIGINT', () => {
+    app.close().then(() => console.log('Gracefull shutdown'));
+  });
+}
+bootstrap();
