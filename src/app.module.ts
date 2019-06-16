5e50d8ea1fc5cc36a38efe531439d47de4d37b9c
Vitali Dziachuk <vitali.dziachuk@itechart-group.com>
2019-06-16 19:48:31 +0300

    vuejs quasar

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { MovieModule } from './movie';
-import { ConfigModule } from './config';
-
-@Module({
-  imports: [MovieModule, ConfigModule],
-})
-export class AppModule {}
--- src/movie/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './movie.controller';
-export * from './movie.module';
-export * from './movie.service';
-export * from './movie.model';
