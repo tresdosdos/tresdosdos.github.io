d257688450c19904fb916bee681bab2130b0f045
Vitaliy Diachuk <vitaliystrelok8@gmail.com>
2019-09-18 21:43:11 +0300

    qwe

---
+++ precache-manifest.9e84c2d4c392138f55c766293118c0ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56fc62909fd0c4cc9f4aa79e57a06b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5aa6e18c0443286480c",
+    "url": "/static/css/main.4de86e7c.chunk.css"
+  },
+  {
+    "revision": "5cd6351118aaf229416d",
+    "url": "/static/js/2.e98f8585.chunk.js"
+  },
+  {
+    "revision": "f5aa6e18c0443286480c",
+    "url": "/static/js/main.fd5223b9.chunk.js"
+  },
+  {
+    "revision": "faae3f44780077b72876",
+    "url": "/static/js/runtime~main.d08bc1d6.js"
+  }
+]);
