2c2016282326875f7de093b79e39c5a9e8ca2f7d
Vitaliy Diachuk <vitaliystrelok8@gmail.com>
2019-09-19 09:37:41 +0300

    qq

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.483bdf0e0b18d08f5c1c69cf96cd71e6.js"
+  "/precache-manifest.abaf1faf4c791fbd790fa693d180c105.js"
 );
 
 self.addEventListener('message', (event) => {
